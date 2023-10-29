237b9f50452d0dc9b9c860388f1aa7b0bf4c6ac0
siavoid <ilya_sysoev_alekseev@mail.ru>
2023-10-30 01:23:16 +0300

    fix

--- internal/controller/http/v1/address.go
+++ internal/controller/http/v1/address.go
@@ -1,1 +1,137 @@
 package v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Miroshinsv/wcharge_back/internal/entity"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+func (s *server) newAddressRoutes() {
+	s.apiRouter.HandleFunc("/address/all", s.GetAddressesWebAPI).Methods(http.MethodGet)
+	s.apiRouter.HandleFunc("/address/get/{id:[0-9]+}", s.GetAddressWebAPI).Methods(http.MethodGet)
+	s.apiRouter.HandleFunc("/address/create", s.CreateAddressWebAPI()).Methods(http.MethodPost)
+	s.apiRouter.HandleFunc("/address/update/{id:[0-9]+}", s.UpdateAddressWebAPI()).Methods(http.MethodPut)
+	s.apiRouter.HandleFunc("/address/delete/{id:[0-9]+}", s.DeleteAddressWebAPI).Methods(http.MethodDelete)
+}
+
+func (s *server) GetAddressesWebAPI(w http.ResponseWriter, r *http.Request) {
+	addresses, err := s.useCase.GetAddresses()
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, fmt.Errorf("GetAddressesWebAPI - %w", err))
+		return
+	}
+
+	s.respond(w, r, http.StatusOK, addresses)
+}
+
+func (s *server) GetAddressWebAPI(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, fmt.Errorf("GetAddressWebAPI - %w", err))
+		return
+	}
+
+	if err != nil {
+		return
+	}
+	st, err := s.useCase.GetAddress(id)
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, fmt.Errorf("GetAddressWebAPI - %w", err))
+		return
+	}
+
+	s.respond(w, r, http.StatusOK, st)
+}
+
+func (s *server) CreateAddressWebAPI() http.HandlerFunc {
+	type request struct {
+		Country string  `json:"country"`
+		City    string  `json:"city"`
+		Address string  `json:"address"`
+		Lat     float64 `json:"lat"`
+		Lng     float64 `json:"lng"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, fmt.Errorf("CreateAddressWebAPI - %w", err))
+			return
+		}
+		st := entity.Address{
+			Country: req.Country,
+			City:    req.City,
+			Address: req.Address,
+			Lat:     req.Lat,
+			Lng:     req.Lng,
+		}
+
+		err := s.useCase.CreateAddress(st)
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, fmt.Errorf("CreateAddressWebAPI - usecase.CreateAddress - %w", err))
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
+	}
+}
+
+func (s *server) UpdateAddressWebAPI() http.HandlerFunc {
+	type request struct {
+		Country string  `json:"country"`
+		City    string  `json:"city"`
+		Address string  `json:"address"`
+		Lat     float64 `json:"lat"`
+		Lng     float64 `json:"lng"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, err := strconv.Atoi(vars["id"])
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, fmt.Errorf("UpdateAddressWebAPI - strconv.Atoi(vars[\"id\"]) - %w", err))
+			return
+		}
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, fmt.Errorf("UpdateAddressWebAPI - %w", err))
+			return
+		}
+		st := entity.Address{
+			Country: req.Country,
+			City:    req.City,
+			Address: req.Address,
+			Lat:     req.Lat,
+			Lng:     req.Lng,
+		}
+
+		err = s.useCase.UpdateAddress(id, st)
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, fmt.Errorf("UpdateAddressWebAPI - useCase.UpdateAddress - %w", err))
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
+	}
+}
+
+func (s *server) DeleteAddressWebAPI(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, fmt.Errorf("DeleteAddressWebAPI - strconv.Atoi(vars[\"id\"]) - %w", err))
+		return
+	}
+
+	err = s.useCase.DeleteAddress(id)
+	if err != nil {
+		s.error(w, r, http.StatusInternalServerError, fmt.Errorf("DeleteAddressWebAPI - usecase.User.GetUsers - %w", err))
+		return
+	}
+
+	s.respond(w, r, http.StatusOK, nil)
+}
--- internal/usecase/repo/postgres/address.go
+++ internal/usecase/repo/postgres/address.go
@@ -1,8 +1,118 @@
 package postgres
 
-import "github.com/Miroshinsv/wcharge_back/internal/entity"
+import (
+	"context"
+	"fmt"
+	"github.com/Masterminds/squirrel"
+	"github.com/Miroshinsv/wcharge_back/internal/entity"
+)
+
+var tableAddress = "tbl_addresses"
+
+func (r *Repo) CreateAddressRepo(addr entity.Address) error {
+	sql, args, err := r.Builder.
+		Insert(tableAddress).
+		Columns("country, city, address, lat, lng").
+		Values(addr.Country, addr.City, addr.Address, addr.Lat, addr.Lng).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - CreateAddressRepo - r.Builder: %w", err)
+	}
+	ctx := context.Background()
+	_, err = r.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - CreateAddressRepo - r.Pool.Exec: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repo) UpdateAddressRepo(id int, addr entity.Address) error {
+	sql, args, err := r.Builder.
+		Update(tableAddress).
+		Set("country", addr.Country).
+		Set("city", addr.City).
+		Set("address", addr.Address).
+		Set("lat", addr.Lat).
+		Set("lng", addr.Lng).
+		Where(squirrel.Eq{"id": id}).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - UpdateAddressRepo - r.Builder: %w", err)
+	}
+	ctx := context.Background()
+	_, err = r.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - UpdateAddressRepo - r.Pool.Query: %w", err)
+	}
+	return nil
+}
+
+func (r *Repo) DeleteAddressRepo(id int) error {
+	sql, args, err := r.Builder.
+		Delete(tableAddress).
+		Where(squirrel.Eq{"id": id}).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - DeleteAddressRepo - r.Builder: %w", err)
+	}
+	ctx := context.Background()
+	_, err = r.Pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("PostgresRepo - DeleteAddressRepo - r.Pool.Query: %w", err)
+	}
+	return nil
+}
 
 func (r *Repo) GetAddressRepo(id int) (entity.Address, error) {
+	u := entity.Address{}
+	sql, args, err := r.Builder.
+		Select("id, country, city, address, lat, lng").
+		From(tableAddress).
+		Where(squirrel.Eq{"id": id}).
+		ToSql()
+	if err != nil {
+		return u, fmt.Errorf("PostgresRepo - GetAddressRepo - r.Builder: %w", err)
+	}
+	ctx := context.Background()
+	row := r.Pool.QueryRow(ctx, sql, args...)
+	if err != nil {
+		return u, fmt.Errorf("PostgresRepo - GetAddressRepo - r.Pool.Query: %w", err)
+	}
 
-	return entity.Address{}, nil
+	err = row.Scan(&u.ID, &u.Country, &u.City, &u.Address, &u.Lat, &u.Lng)
+	if err != nil {
+		return entity.Address{}, fmt.Errorf("AddressRepo - GetAddresss - rows.Scan: %w", err)
+	}
+
+	return u, nil
+}
+
+func (r *Repo) GetAddressesRepo() ([]entity.Address, error) {
+	sql, _, err := r.Builder.
+		Select("id, country, city, address, lat, lng").
+		From(tableAddress).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("GetAddresses - r.Builder: %w", err)
+	}
+	ctx := context.Background()         //!!!
+	rows, err := r.Pool.Query(ctx, sql) //!!!
+	if err != nil {
+		return nil, fmt.Errorf("GetAddresses - r.Pool.Query: %w", err)
+	}
+	defer rows.Close()
+
+	entities := make([]entity.Address, 0, _defaultEntityCap)
+
+	for rows.Next() {
+		u := entity.Address{}
+		err = rows.Scan(&u.ID, &u.Country, &u.City, &u.Address, &u.Lat, &u.Lng)
+		if err != nil {
+			return nil, fmt.Errorf("AddressRepo - GetAddresss - rows.Scan: %w", err)
+		}
+		entities = append(entities, u)
+	}
+
+	return entities, nil
 }
